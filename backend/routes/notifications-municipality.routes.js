ef859f21f47483a57d54217f4579913f7deef6cc
Aridane1 <aridanejdr6@gmail.com>
2023-12-03 00:22:07 +0000

    issue#37

--- backend/routes/notifications-municipality.routes.js
+++ backend/routes/notifications-municipality.routes.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
   const notifications = require("../controllers/notifications-municipality.controller.js");
   const router = require("express").Router();
-  router.get("/:id", notifications.getNotificationByLocation);
+  router.get("/", notifications.getNotificationByLocation);
   router.post("/", notifications.createOneNotificationMunicipality);
   router.post("/forall", notifications.createManyNotificationMunicipality);
   router.delete(
